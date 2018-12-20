5a54ea6b00340bdb9d544cb8c1b2875477f8a35c
Radiaci9 <cvirko1998@mail.ru>
2018-12-20 15:42:54 +0300

    Updates

---
+++ precache-manifest.d2a65e11e8299ae2d00817979481ca4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0668a4d3ddcc7d25b004",
+    "url": "/react-app-lab6/static/js/runtime~main.0668a4d3.js"
+  },
+  {
+    "revision": "4f85c174e1cdcbf0c230",
+    "url": "/react-app-lab6/static/js/main.4f85c174.chunk.js"
+  },
+  {
+    "revision": "37f6903880e0c98685bf",
+    "url": "/react-app-lab6/static/js/1.37f69038.chunk.js"
+  },
+  {
+    "revision": "4f85c174e1cdcbf0c230",
+    "url": "/react-app-lab6/static/css/main.acf8a81d.chunk.css"
+  },
+  {
+    "revision": "37f6903880e0c98685bf",
+    "url": "/react-app-lab6/static/css/1.53bc3849.chunk.css"
+  },
+  {
+    "revision": "5fe9b67e51627b0c567e6c14cb85161b",
+    "url": "/react-app-lab6/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/lab6/precache-manifest.b59b56efd065e597a4ffec728fd2e981.js"
+  "/react-app-lab6/precache-manifest.d2a65e11e8299ae2d00817979481ca4a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/lab6/index.html", {
+workbox.routing.registerNavigationRoute("/react-app-lab6/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
